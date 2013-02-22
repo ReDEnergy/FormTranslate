1ab4c3931f21cf3b94d1cd337a93c534dfba9fea
Gabriel Ivanica <ivanicagabriel@gmail.com>
2013-02-22 04:09:30 +0200

    implemented tab worker for communications

--- data/formtranslateUI.js
+++ data/formtranslateUI.js
@@ -72,53 +72,6 @@
 		
 	}
 	
-
-	var Events = {
-		
-		text : null,
-		elem : null,
-		timeoutID : null,
-		get_value : true,
-		
-		inputChanged: function (e) {
-
-			if (Events.get_value)
-				Events.text = e.target.value;
-			else
-				Events.text = e.target.textContent;	
-
-			console.log("text: ", Events.text);
-
-			clearTimeout(Events.timeoutID);
-			Events.timeoutID = setTimeout( Events.translateText, 1000);
-			
-		},			
-
-		translateText : function () {
-			console.log("Sent Translate Query: ", Events.text);
-			// self.port.emit('translate', text);
-		},
-
-		startListen : function () {
-			
-			this.elem = document.querySelector("[formtranslateid=true]");
-
-			console.log("Start listen on ", this.elem.id);
-
-			if (this.elem.nodeName !== "INPUT" && this.elem.nodeName !== "TEXTAREA")
-				this.get_value = false;
-				
-			console.log(this.get_value);
-				
-			this.elem.addEventListener('input', Events.inputChanged);
-		},
-
-		stopListen : function () {
-			this.elem.removeEventListener('input', Events.inputChanged);
-		}		
-	}
-	
-
 	self.port.on("response", function(response) {
 		console.log("Response received: ", response);		
 		translation.textContent = response;
@@ -126,7 +79,6 @@
 
 	self.port.on("init", function() {
 		UI.init();
-		Events.startListen();
 	});
 	
 	self.port.on("stop", function() {
